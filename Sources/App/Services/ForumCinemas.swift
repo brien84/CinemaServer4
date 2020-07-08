e5691a642a1356fe9ae72f0e52650d88e7576a93
brien84 <houseinblues@gmail.com>
2020-07-08 19:09:12 +0300

    Create `ForumCinemas` service

---
+++ Sources/App/Services/ForumCinemas.swift
@@ -0,0 +1,123 @@
+//
+//  ForumCinemas.swift
+//  
+//
+//  Created by Marius on 2020-07-07.
+//
+
+import Vapor
+
+struct ForumCinemas {
+    private let client: Client
+
+    init(client: Client) {
+        self.client = client
+    }
+
+    func start() {
+        getShowings().whenSuccess { print($0.count) }
+    }
+
+    private func getShowings() -> EventLoopFuture<[ForumShowing]> {
+        return getAreas().flatMap { areas in
+            areas.map { area in
+                self.getShowings(in: area)
+            }.flatten(on: self.client.eventLoop).map { $0.flatMap { $0 } }
+        }
+    }
+
+    private func getAreas() -> EventLoopFuture<[Area]> {
+        let uri = URI(string: "http://m.forumcinemas.lt/xml/TheatreAreas/?format=json")
+
+        return client.get(uri).map { res in
+            do {
+                let service = try JSONDecoder().decode(AreaService.self, from: res.body ?? ByteBuffer())
+                return service.areas
+            } catch {
+                print("LOG: \(error)")
+                return []
+            }
+        }
+    }
+
+    private func getShowings(in area: Area) -> EventLoopFuture<[ForumShowing]> {
+        let uri = URI(string: "http://m.forumcinemas.lt/xml/Schedule/?format=json&nrOfDays=31&area=\(area.id)")
+
+        return client.get(uri).map { res in
+            do {
+                let service = try JSONDecoder().decode(ShowingService.self, from: res.body ?? ByteBuffer())
+
+                // Assigns `area` to each `ForumShowing` object.
+                let showings = service.showings.map { showing -> ForumShowing in
+                    var copy = showing
+                    copy.area = area
+                    return copy
+                }
+
+                return showings
+            } catch {
+                print("LOG: \(error)")
+                return []
+            }
+        }
+    }
+
+    // MARK: - Decodable Helpers
+
+    private struct ForumShowing: Decodable {
+        let title: String
+        let originalTitle: String
+        let year: Int
+        let ageRating: String
+        let duration: Int
+        let genres: String
+        let date: String
+        let venue: String
+        let url: String
+        var area: Area?
+
+        private enum CodingKeys: String, CodingKey {
+            case title = "Title"
+            case originalTitle = "OriginalTitle"
+            case year = "ProductionYear"
+            case ageRating = "RatingLabel"
+            case duration = "LengthInMinutes"
+            case genres = "Genres"
+            case date = "dttmShowStart"
+            case venue = "Theatre"
+            case url = "ShowURL"
+        }
+    }
+
+    private struct ShowingService: Decodable {
+        let showings: [ForumShowing]
+
+        private enum CodingKeys: String, CodingKey {
+             case showings = "Shows"
+        }
+    }
+
+    private struct Area: Decodable {
+        let id: Int
+        let name: String
+
+        private enum CodingKeys: String, CodingKey {
+            case id = "ID"
+            case name = "Name"
+        }
+    }
+
+    private struct AreaService: Decodable {
+        let areas: [Area]
+
+        private enum CodingKeys: String, CodingKey {
+            case areas = "TheatreAreas"
+        }
+    }
+}
+
+extension Application {
+    var forumCinemas: ForumCinemas {
+        .init(client: self.client)
+    }
+}
