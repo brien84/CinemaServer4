68cd3fdd45cc89627019e3499b7dc12a09c81fa5
brien84 <houseinblues@gmail.com>
2020-07-10 23:26:11 +0300

    Create `Cinamon` service

---
+++ Sources/App/Services/Cinamon.swift
@@ -0,0 +1,138 @@
+//
+//  Cinamon.swift
+//  
+//
+//  Created by Marius on 2020-07-09.
+//
+
+import Vapor
+
+struct Cinamon {
+    private let client: Client
+
+    init(client: Client) {
+        self.client = client
+    }
+
+    func getMovies() -> EventLoopFuture<[Movie]> {
+        client.get(uri).map { res in
+            do {
+                let service = try JSONDecoder().decode(CinamonService.self, from: res.body ?? ByteBuffer())
+
+                return service.movies.map { movie in
+                    Movie(from: movie, on: service.screens)
+                }
+            } catch {
+                print("LOG: \(error)")
+                return []
+            }
+        }
+    }
+}
+
+extension Cinamon {
+    private var uri: URI {
+        URI(string: "https://cinamonkino.com/api/page/movies?cinema_id=77139293&timezone=Europe%2FTallinn&locale=lt")
+    }
+}
+
+extension Application {
+    var cinamon: Cinamon {
+        .init(client: self.client)
+    }
+}
+
+extension Movie {
+    fileprivate convenience init(from movie: CinamonMovie, on screens: [String]) {
+        let year: String? = {
+            guard let substring = movie.year?.split(separator: "-").first else { return nil }
+            return String(substring)
+        }()
+
+        let duration: String? = {
+            guard let movieDuration = movie.duration else { return nil }
+            return String(movieDuration) + " min"
+        }()
+
+        let genres: [String]? = {
+            guard let genre = movie.genre?.name else { return nil }
+            return [genre]
+        }()
+
+        let showings = movie.showings.compactMap { showing -> Showing? in
+            if screens.contains(showing.screen) {
+                return Showing(from: showing)
+            } else {
+                return nil
+            }
+        }
+
+        self.init(title: movie.title,
+                  originalTitle: movie.originalTitle,
+                  year: year,
+                  duration: duration,
+                  ageRating: movie.ageRating,
+                  genres: genres,
+                  plot: nil,
+                  poster: nil,
+                  showings: showings)
+    }
+}
+
+extension Showing {
+    fileprivate convenience init?(from showing: CinamonShowing) {
+        guard let date = showing.showtime.convertToDate() else { return nil }
+
+        self.init(city: "Kaunas",
+                  date: date,
+                  venue: "Cinamon",
+                  is3D: showing.is3D,
+                  url: "https://cinamonkino.com/mega/seat-plan/\(showing.pid)/lt")
+    }
+}
+
+private struct CinamonService: Decodable {
+    let movies: [CinamonMovie]
+
+    // `Screens` contains local theater IDs, which are used to filter out local showings,
+    // since API returns showings from theaters across multiple countries.
+    let screens: [String]
+}
+
+private struct CinamonMovie: Decodable {
+    let title: String?
+    let originalTitle: String?
+    let year: String?
+    let duration: Int?
+    let ageRating: String?
+    let genre: Genre?
+    let showings: [CinamonShowing]
+
+    struct Genre: Decodable {
+        let name: String
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case title = "name"
+        case originalTitle = "original_name"
+        case year = "premiere_date"
+        case duration = "runtime"
+        case ageRating = "rating"
+        case genre
+        case showings = "sessions"
+    }
+}
+
+private struct CinamonShowing: Decodable {
+    let pid: Int
+    let screen: String
+    let showtime: String
+    let is3D: Bool
+
+    private enum CodingKeys: String, CodingKey {
+        case pid
+        case screen = "screen_name"
+        case showtime
+        case is3D = "is_3d"
+    }
+}
