fa67aecf750a0df2fdec9dbdc0252380e9a459da
brien84 <houseinblues@gmail.com>
2020-07-12 23:30:05 +0300

    Add `getMovies()` method

--- Sources/App/Controllers/MainController.swift
+++ Sources/App/Controllers/MainController.swift
@@ -10,11 +10,36 @@ import Vapor
 struct MainController {
     private var app: Application
 
+    private var forum: ForumCinemas
+    private var multikino: Multikino
+    private var cinamon: Cinamon
+
     init(app: Application) {
         self.app = app
+        self.forum = app.forumCinemas
+        self.multikino = app.multikino
+        self.cinamon = app.cinamon
     }
 
     func start() {
-        print("STARTING!")
+        getMovies().whenSuccess {
+            print($0.count)
+        }
+    }
+
+    private func getMovies() -> EventLoopFuture<[Movie]> {
+        forum.getMovies().flatMap { forumMovies in
+            self.multikino.getMovies().flatMap { multiMovies in
+                self.cinamon.getMovies().map { cinamonMovies -> [Movie] in
+                    var movies = [Movie]()
+
+                    movies.append(contentsOf: forumMovies)
+                    movies.append(contentsOf: multiMovies)
+                    movies.append(contentsOf: cinamonMovies)
+
+                    return movies
+                }
+            }
+        }
     }
 }
