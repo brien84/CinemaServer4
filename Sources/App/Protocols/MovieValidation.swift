3b5cd80f8f0c59dcc08552dc61a130d733b1b821
brien84 <houseinblues@gmail.com>
2020-07-13 22:31:44 +0300

    Create `MovieValidation` protocol

---
+++ Sources/App/Protocols/MovieValidation.swift
@@ -0,0 +1,73 @@
+//
+//  MovieValidation.swift
+//  
+//
+//  Created by Marius on 2020-07-13.
+//
+
+import Vapor
+
+protocol MovieValidation: AnyObject {
+    var validationReport: String { get set }
+
+    func validate(_ movie: Movie) -> Movie?
+}
+
+extension MovieValidation {
+    func validate(_ movie: Movie) -> Movie? {
+        var didFailValidation = false
+
+        appendReport(with: "")
+
+        guard let originalTitle = movie.originalTitle else {
+            appendReport(with: "`originalTitle` is nil!")
+            return nil
+        }
+
+        if movie.title == nil {
+            didFailValidation = true
+            appendReport(with: "\(originalTitle) `title` is nil!")
+        }
+
+        if movie.year == nil {
+            didFailValidation = true
+            appendReport(with: "\(originalTitle) `year` is nil!")
+        }
+
+        if movie.duration == nil {
+            didFailValidation = true
+            appendReport(with: "\(originalTitle) `duration` is nil!")
+        }
+
+        if movie.ageRating == nil {
+            didFailValidation = true
+            appendReport(with: "\(originalTitle) `ageRating` is nil!")
+        }
+
+        if movie.genres == nil {
+            didFailValidation = true
+            appendReport(with: "\(originalTitle) `genres` is nil!")
+        }
+
+        if movie.plot == nil {
+            didFailValidation = true
+            appendReport(with: "\(originalTitle) `plot` is nil!")
+        }
+
+        if movie.poster == nil {
+            didFailValidation = true
+            appendReport(with: "\(originalTitle) `poster` is nil!")
+        }
+
+        if movie.showings.isEmpty {
+            didFailValidation = true
+            appendReport(with: "\(originalTitle) `showings` is empty!")
+        }
+
+        return didFailValidation ? nil : movie
+    }
+
+    private func appendReport(with message: String) {
+        validationReport.append(contentsOf: message + "\n")
+    }
+}
