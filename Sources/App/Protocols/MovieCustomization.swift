ab621a70e2c32e980de5db83db8605312a875e61
brien84 <houseinblues@gmail.com>
2020-07-13 18:08:28 +0300

    Add `applyProfile(to movie: Movie)` method requirement and create default implementation

--- Sources/App/Protocols/MovieCustomization.swift
+++ Sources/App/Protocols/MovieCustomization.swift
@@ -10,12 +10,59 @@ import Vapor
 protocol MovieCustomization {
     var cinemaIdentifier: String { get }
 
+    func applyProfile(to movie: Movie) -> Movie
     func customizeOriginalTitle(for movie: Movie) -> Movie
 }
 
 extension MovieCustomization {
-    // Modifies `Movie`s `originalTitle` property by replacing provided values
-    // with new values from `OriginalTitles.plist` file.
+    // Replaces values of `Movie` properties with new values from `Profiles.plist` file.
+    func applyProfile(to movie: Movie) -> Movie {
+        guard let profiles = loadProfiles() else {
+            print("Could not open `Profiles.plist`!")
+            return movie
+        }
+
+        guard let originalTitle = movie.originalTitle else { return movie }
+        guard let profile = profiles[originalTitle] as? [String: AnyObject] else { return movie }
+
+        if let title = profile["title"] as? String {
+            movie.title = title
+        }
+
+        if let year = profile["year"] as? String {
+            movie.year = year
+        }
+
+        if let duration = profile["duration"] as? String {
+            movie.duration = duration
+        }
+
+        if let ageRating = profile["ageRating"] as? String {
+            movie.ageRating = ageRating
+        }
+
+        if let genres = profile["genres"] as? [String] {
+            movie.genres = genres
+        }
+
+        if let plot = profile["plot"] as? String {
+            movie.plot = plot
+        }
+
+        return movie
+    }
+
+    private func loadProfiles() -> [String: AnyObject]? {
+        let publicDirectory = DirectoryConfiguration.detect().publicDirectory
+        let url = URL(fileURLWithPath: publicDirectory + "Profiles.plist")
+        guard let data = try? Data(contentsOf: url) else { return nil }
+
+        return try? PropertyListSerialization.propertyList(from: data,
+                                                           options: .mutableContainers,
+                                                           format: .none) as? [String: AnyObject]
+    }
+
+    // Replaces `Movie`s `originalTitle` property with new value from `OriginalTitles.plist` file.
     func customizeOriginalTitle(for movie: Movie) -> Movie {
         guard let originalTitles = readOriginalTitlesPlist() else {
             print("Could not open `OriginalTitles.plist`!")
