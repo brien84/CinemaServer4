118ebf5fba1fd6be211ada8fd3dc8086dbc425aa
brien84 <houseinblues@gmail.com>
2020-07-15 18:02:22 +0300

    Add `all` route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,7 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req in
-        return "It works!"
-    }
-
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+    app.get("all") { req in
+        Movie.query(on: req.db).all()
     }
 }
