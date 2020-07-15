c0ae4f6659262df2068440b6482034b5670bcfda
brien84 <houseinblues@gmail.com>
2020-07-15 23:29:45 +0300

    Create `posters` route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,4 +4,11 @@ func routes(_ app: Application) throws {
     app.get("all") { req in
         Movie.query(on: req.db).all()
     }
+
+    app.get("posters", ":fileName") { req -> Response in
+        let fileName = req.parameters.get("fileName")
+        let path = "\(DirectoryConfiguration.detect().publicDirectory)/Posters" + (fileName ?? "")
+
+        return req.fileio.streamFile(at: path)
+    }
 }
