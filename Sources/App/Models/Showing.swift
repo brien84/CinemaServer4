ab9b1214aa378917ef1aec42a283e94c2d09f6c0
brien84 <houseinblues@gmail.com>
2020-07-15 21:33:52 +0300

    Unconform `Showing` from `Model` protocol

--- Sources/App/Models/Showing.swift
+++ Sources/App/Models/Showing.swift
@@ -5,31 +5,15 @@
 //  Created by Marius on 2020-07-07.
 //
 
-import Fluent
 import Vapor
 
-final class Showing: Model, Content {
-    static let schema = "showings"
-
-    @ID(key: .id)
-    var id: UUID?
-
-    @Field(key: "city")
-    var city: String
-
-    @Field(key: "date")
-    var date: Date
-
-    @Field(key: "venue")
-    var venue: String
-
-    @Field(key: "is3D")
-    var is3D: Bool
-
-    @Field(key: "url")
-    var url: String
-
-    init() { }
+final class Showing: Content {
+    let id: UUID?
+    let city: String
+    let date: Date
+    let venue: String
+    let is3D: Bool
+    let url: String
 
     init(id: UUID? = nil, city: String, date: Date, venue: String, is3D: Bool, url: String) {
         self.id = id
@@ -38,22 +22,5 @@ final class Showing: Model, Content {
         self.venue = venue
         self.is3D = is3D
         self.url = url
-    }
-}
-
-struct CreateShowings: Migration {
-    func prepare(on database: Database) -> EventLoopFuture<Void> {
-        database.schema("showings")
-            .id()
-            .field("city", .string, .required)
-            .field("date", .date, .required)
-            .field("venue", .string, .required)
-            .field("is3D", .bool, .required)
-            .field("url", .string, .required)
-            .create()
-    }
-
-    func revert(on database: Database) -> EventLoopFuture<Void> {
-        database.schema("showings").delete()
     }
 }
