d6be1eed833efafd48b1ffa74c9601ee50c027ae
brien84 <houseinblues@gmail.com>
2020-07-16 18:44:32 +0300

    Fix `path` property in `posters` route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,8 +7,7 @@ func routes(_ app: Application) throws {
 
     app.get("posters", ":fileName") { req -> Response in
         let fileName = req.parameters.get("fileName")
-        let path = "\(DirectoryConfiguration.detect().publicDirectory)/Posters" + (fileName ?? "")
-
+        let path = "\(DirectoryConfiguration.detect().publicDirectory)Posters/" + (fileName ?? "")
         return req.fileio.streamFile(at: path)
     }
 }
