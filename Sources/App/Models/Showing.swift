df5fc5cdfc89ad7a98789272f3218809a72547fa
brien84 <houseinblues@gmail.com>
2020-10-14 19:29:57 +0300

    Refactor `Showing` to conform to `Fluent` `Model` protocol

--- Sources/App/Models/Showing.swift
+++ Sources/App/Models/Showing.swift
@@ -5,15 +5,34 @@
 //  Created by Marius on 2020-07-07.
 //
 
+import Fluent
 import Vapor
 
-final class Showing: Content {
-    let id: UUID?
-    let city: String
-    let date: Date
-    let venue: String
-    let is3D: Bool
-    let url: String
+final class Showing: Model, Content {
+    static let schema = "showings"
+
+    @ID(key: .id)
+    var id: UUID?
+
+    @Field(key: "city")
+    var city: String
+
+    @Field(key: "date")
+    var date: Date
+
+    @Field(key: "venue")
+    var venue: String
+
+    @Field(key: "is_3D")
+    var is3D: Bool
+
+    @Field(key: "url")
+    var url: String
+
+    @Parent(key: "movie_id")
+    var movie: Movie
+
+    init() { }
 
     init(id: UUID? = nil, city: String, date: Date, venue: String, is3D: Bool, url: String) {
         self.id = id
@@ -22,5 +41,24 @@ final class Showing: Content {
         self.venue = venue
         self.is3D = is3D
         self.url = url
+    }
+}
+
+struct CreateShowings: Migration {
+    func prepare(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("showings")
+            .id()
+            .field("city", .string)
+            .field("date", .datetime)
+            .field("venue", .string)
+            .field("is_3D", .bool)
+            .field("url", .string)
+            .field("movie_id", .uuid, .references("movies", "id"))
+            .foreignKey("movie_id", references: "movies", "id", onDelete: .cascade)
+            .create()
+    }
+
+    func revert(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("showings").delete()
     }
 }
