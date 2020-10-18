062bf7065614d6291444f6100a1066004e166c49
brien84 <houseinblues@gmail.com>
2020-10-18 22:53:19 +0300

    Test `fetchMovies` method

--- Tests/AppTests/MultikinoTests.swift
+++ Tests/AppTests/MultikinoTests.swift
@@ -1,6 +1,6 @@
 //
 //  MultikinoTests.swift
-//  
+//
 //
 //  Created by Marius on 2020-09-26.
 //
@@ -19,5 +19,35 @@ final class MultikinoTests: XCTestCase {
     override func tearDown() {
         sut = nil
         app.shutdown()
+    }
+
+    func testFetchingNoResponseThrowsError() throws {
+        let client = ClientStub(eventLoop: app.eventLoopGroup.next(), testData: .noResponse)
+        sut = Multikino(client: client, database: app.db)
+
+        XCTAssertThrowsError(try sut.fetchMovies().wait())
+    }
+
+    func testFetchingValidResponse() throws {
+        let client = ClientStub(eventLoop: app.eventLoopGroup.next(), testData: .multikinoValid)
+        sut = Multikino(client: client, database: app.db)
+
+        try sut.fetchMovies().wait()
+        
+        let movies = try Movie.query(on: app.db).with(\.$showings).all().wait()
+        XCTAssertGreaterThan(movies.count, 0)
+        movies.forEach { XCTAssertGreaterThan($0.showings.count, 0) }
+    }
+
+    /// If response contains bad data (missing properties, incorrect values), decoder should ignore it and not throw error.
+    func testFetchingBadDataDoesNotThrow() throws {
+        let client = ClientStub(eventLoop: app.eventLoopGroup.next(), testData: .multikinoBadData)
+        sut = Multikino(client: client, database: app.db)
+
+        try sut.fetchMovies().wait()
+
+        let movies = try Movie.query(on: app.db).with(\.$showings).all().wait()
+        XCTAssertGreaterThan(movies.count, 0)
+        movies.forEach { XCTAssertGreaterThan($0.showings.count, 0)}
     }
 }
