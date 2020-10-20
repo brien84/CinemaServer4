64f64a99ba110f352bff14a3f6fe0488798e6673
brien84 <houseinblues@gmail.com>
2020-10-20 23:01:00 +0300

    Test `fetchMovies` method

--- Tests/AppTests/ForumCinemasTests.swift
+++ Tests/AppTests/ForumCinemasTests.swift
@@ -1,6 +1,6 @@
 //
 //  ForumCinemasTests.swift
-//  
+//
 //
 //  Created by Marius on 2020-09-26.
 //
@@ -19,5 +19,43 @@ final class ForumCinemasTests: XCTestCase {
     override func tearDown() {
         sut = nil
         app.shutdown()
+    }
+
+    /// `getAreas()` method should throw.
+    func testFetchingNoResponseThrowsError() throws {
+        let client = ClientStub(eventLoop: app.eventLoopGroup.next(), testData: .noResponse)
+        sut = ForumCinemas(client: client, database: app.db)
+
+        XCTAssertThrowsError(try sut.fetchMovies().wait())
+    }
+
+    /// `forumCinemasNoShowings` test data only contains a valid list of areas, so
+    /// `getForumShowings(in area: Area)`  method should throw.
+    func testFetchingNoShowingsResponseThrowsError() throws {
+        let client = ClientStub(eventLoop: app.eventLoopGroup.next(), testData: .forumCinemasNoShowings)
+        sut = ForumCinemas(client: client, database: app.db)
+
+        XCTAssertThrowsError(try sut.fetchMovies().wait())
+    }
+
+    func testFetchingValidResponse() throws {
+        let client = ClientStub(eventLoop: app.eventLoopGroup.next(), testData: .forumCinemasValid)
+        sut = ForumCinemas(client: client, database: app.db)
+
+        try sut.fetchMovies().wait()
+
+        let movies = try Movie.query(on: app.db).with(\.$showings).all().wait()
+        XCTAssertGreaterThan(movies.count, 0)
+        movies.forEach { XCTAssertGreaterThan($0.showings.count, 0) }
+    }
+
+    /// If response contains bad data (missing properties, incorrect values), decoder should ignore it and not throw error.
+    func testFetchingBadDataDoesNotThrow() throws {
+        let client = ClientStub(eventLoop: app.eventLoopGroup.next(), testData: .forumCinemasBadData)
+        sut = ForumCinemas(client: client, database: app.db)
+
+        try sut.fetchMovies().wait()
+        let movies = try Movie.query(on: app.db).with(\.$showings).all().wait()
+        XCTAssertGreaterThan(movies.count, 0)
     }
 }
