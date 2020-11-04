3041573132d4636e46d5ebf97792008ffa78a26a
brien84 <houseinblues@gmail.com>
2020-11-04 19:52:28 +0200

    Create `TitleMapping` class

---
+++ Sources/App/Models/TitleMapping.swift
@@ -0,0 +1,47 @@
+//
+//  TitleMapping.swift
+//  
+//
+//  Created by Marius on 2020-10-21.
+//
+
+import Fluent
+import Vapor
+
+/// Record of `originalTitle` which should be replaced with `newOriginalTitle`,
+/// since same `Movie` can have non-identical names in different cinemas.
+///
+/// `TitleMappings` should be added to the database manually.
+final class TitleMapping: Model, Content {
+    static let schema = "title_mappings"
+
+    @ID(key: .id)
+    var id: UUID?
+
+    @Field(key: "original_title")
+    var originalTitle: String
+
+    @Field(key: "new_original_title")
+    var newOriginalTitle: String
+
+    convenience init(originalTitle: String, newOriginalTitle: String) {
+        self.init()
+
+        self.originalTitle = originalTitle
+        self.newOriginalTitle = newOriginalTitle
+    }
+}
+
+struct CreateTitleMappings: Migration {
+    func prepare(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("title_mappings")
+            .id()
+            .field("original_title", .string, .required)
+            .field("new_original_title", .string, .required)
+            .create()
+    }
+
+    func revert(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("title_mappings").delete()
+    }
+}
