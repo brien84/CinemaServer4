9a029fedf3fc0f25759d9b05803074d2acfacb4a
brien84 <houseinblues@gmail.com>
2020-11-04 22:39:55 +0200

    Add title mapping methods

--- Sources/App/Services/MovieOrganizer.swift
+++ Sources/App/Services/MovieOrganizer.swift
@@ -1,12 +1,36 @@
 //
 //  MovieOrganizer.swift
-//  
+//
 //
 //  Created by Marius on 2020-10-25.
 //
 
+import Fluent
 import Vapor
 
 struct MovieOrganizer {
+
+    private func mapOriginalTitles(on db: Database) -> EventLoopFuture<Void> {
+        Movie.query(on: db).all().flatMap { movies in
+            movies.map { movie in
+                self.mapOriginalTitle(on: movie, on: db)
+            }.flatten(on: db.eventLoop)
+        }
+    }
+
+    private func mapOriginalTitle(on movie: Movie, on db: Database) -> EventLoopFuture<Void> {
+        guard let title = movie.originalTitle else {
+            return db.eventLoop.makeSucceededFuture(())
+        }
+
+        return TitleMapping.query(on: db).filter(\.$originalTitle == title).first().flatMap { mapping in
+            guard let mapping = mapping else {
+                return db.eventLoop.makeSucceededFuture(())
+            }
+
+            movie.originalTitle = mapping.newOriginalTitle
+            return movie.update(on: db)
+        }
+    }
 
 }
