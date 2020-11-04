5d851b712ceb781420a9d7b68dd106169242e7bd
brien84 <houseinblues@gmail.com>
2020-11-04 22:53:31 +0200

    Add `MovieProfile` applying tests

--- Tests/AppTests/MovieOrganizerTests.swift
+++ Tests/AppTests/MovieOrganizerTests.swift
@@ -1,6 +1,6 @@
 //
 //  MovieOrganizerTests.swift
-//  
+//
 //
 //  Created by Marius on 2020-10-25.
 //
@@ -36,4 +36,69 @@ final class MovieOrganizerTests: XCTestCase {
         XCTAssertEqual(movies[0].originalTitle, newTitle)
     }
 
+    func testMovieProfileGetsCreated() throws {
+        let title = "TestTitle"
+        let originalTitle = "TestTitle"
+        let year = "TestYear"
+        let duration = "TestDuration"
+        let ageRating = "TestAgeRating"
+        let genres = ["TestGenre"]
+        let plot = "TestPlot"
+
+        Movie.create(title: title, originalTitle: originalTitle, year: year, duration: duration,
+                     ageRating: ageRating, genres: genres, plot: plot, poster: "", on: app.db)
+
+        _ = try sut.organize(on: app.db).wait()
+
+        let profile = try MovieProfile.query(on: app.db).first().wait()
+
+        XCTAssertEqual(profile?.title, title)
+        XCTAssertEqual(profile?.originalTitle, originalTitle)
+        XCTAssertEqual(profile?.year, year)
+        XCTAssertEqual(profile?.duration, duration)
+        XCTAssertEqual(profile?.ageRating, ageRating)
+        XCTAssertEqual(profile?.genres, genres)
+        XCTAssertEqual(profile?.plot, plot)
+    }
+
+    func testNewProfileIsNotCreatedIfOneAlreadyExists() throws {
+        let originalTitle = "TestTitle"
+        MovieProfile.create(title: "", originalTitle: originalTitle, year: "",
+                            duration: "", ageRating: "", genres: [], plot: "", on: app.db)
+        Movie.create(title: "", originalTitle: originalTitle, year: "",
+                     duration: "", ageRating: "", genres: [], plot: "", poster: "", on: app.db)
+
+        _ = try sut.organize(on: app.db).wait()
+
+        let count = try MovieProfile.query(on: app.db).count().wait()
+        XCTAssertEqual(count, 1)
+    }
+
+    func testApplyingProfiles() throws {
+        let title = "TestTitle"
+        let originalTitle = "TestTitle"
+        let year = "TestYear"
+        let duration = "TestDuration"
+        let ageRating = "TestAgeRating"
+        let genres = ["TestGenre"]
+        let plot = "TestPlot"
+
+        Movie.create(title: "", originalTitle: originalTitle, year: "",
+                     duration: "", ageRating: "", genres: [""], plot: "", on: app.db)
+        MovieProfile.create(title: title, originalTitle: originalTitle, year: year,
+                            duration: duration, ageRating: ageRating, genres: genres, plot: plot, on: app.db)
+
+        _ = try sut.organize(on: app.db).wait()
+
+        let movies = try Movie.query(on: app.db).all().wait()
+
+        XCTAssertEqual(movies.count, 1)
+        XCTAssertEqual(movies[0].title, title)
+        XCTAssertEqual(movies[0].originalTitle, originalTitle)
+        XCTAssertEqual(movies[0].year, year)
+        XCTAssertEqual(movies[0].duration, duration)
+        XCTAssertEqual(movies[0].ageRating, ageRating)
+        XCTAssertEqual(movies[0].genres, genres)
+        XCTAssertEqual(movies[0].plot, plot)
+    }
 }
