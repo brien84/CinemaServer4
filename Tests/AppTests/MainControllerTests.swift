da618168a5f84f9c1adf75c52cd55d69e372890c
brien84 <houseinblues@gmail.com>
2020-11-05 14:50:18 +0200

    Add email tests

---
+++ Tests/AppTests/MainControllerTests.swift
@@ -0,0 +1,75 @@
+//
+//  MainControllerTests.swift
+//  
+//
+//  Created by Marius on 2020-10-04.
+//
+
+@testable import App
+import Fluent
+import SendGrid
+import XCTVapor
+
+final class MainControllerTests: XCTestCase {
+    var app: Application!
+    var sut: MainController!
+    var fetcher: TestFetcher!
+    var sender: TestSender!
+
+    override func setUp() {
+        app = try! Application.testable()
+        fetcher = TestFetcher()
+        sender = TestSender(eventLoop: app.eventLoopGroup.next())
+        sut = MainController(app: app, fetcher: fetcher, sender: sender)
+    }
+
+    override func tearDown() {
+        sut = nil
+        app.shutdown()
+    }
+
+    func testEmailConfiguration() throws {
+        try sut.update().wait()
+
+        let email = sender.sentEmail
+
+        XCTAssertEqual(email?.from?.email, Config.emailAddress)
+        XCTAssertEqual(email?.personalizations?.first?.to?.first?.email, Config.emailAddress)
+        XCTAssertEqual(email?.subject, "Validation report")
+    }
+
+    // MARK: Test Helpers
+
+    enum TestError: Error {
+        case error
+    }
+
+    class TestFetcher: MovieFetching {
+        var isSuccess = true
+
+        func fetch(on db: Database) -> EventLoopFuture<Void> {
+            isSuccess ? db.eventLoop.makeSucceededFuture(()) : db.eventLoop.makeFailedFuture(TestError.error)
+        }
+    }
+
+    class TestSender: EmailSending {
+        var eventLoop: EventLoop
+        var sentEmail: SendGridEmail?
+
+        init(eventLoop: EventLoop) {
+            self.eventLoop = eventLoop
+        }
+
+        func send(email: SendGridEmail) -> EventLoopFuture<Void> {
+            sentEmail = email
+            return eventLoop.makeSucceededFuture(())
+        }
+
+        func getSentEmailContent() -> String {
+            guard let content = sentEmail?.content?.first?["value"]
+                else { XCTFail("Could not get email content!"); return "" }
+
+            return content
+        }
+    }
+}
