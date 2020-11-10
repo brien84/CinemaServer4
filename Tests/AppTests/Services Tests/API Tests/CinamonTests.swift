23df8148c056dcdfd92f2f8601e56a1e83b1f059
brien84 <houseinblues@gmail.com>
2020-11-10 19:59:40 +0200

    Refactor `CinamonTests`

--- Tests/AppTests/Services Tests/API Tests/CinamonTests.swift
+++ Tests/AppTests/Services Tests/API Tests/CinamonTests.swift
@@ -21,34 +21,193 @@ final class CinamonTests: XCTestCase {
         app.shutdown()
     }
 
-    func testFetchingNoResponseThrowsError() throws {
-        let client = ClientStub(eventLoop: app.eventLoopGroup.next(), testData: .noResponse)
+    func testFetchingBadResponseThrowsError() throws {
+        let client = ClientStub(eventLoop: app.eventLoopGroup.next(), data: nil)
         sut = Cinamon(client: client)
 
         XCTAssertThrowsError(try sut.fetchMovies(on: app.db).wait())
     }
 
-    func testFetchingValidResponse() throws {
-        let client = ClientStub(eventLoop: app.eventLoopGroup.next(), testData: .cinamonValid)
+    func testFetching() throws {
+        let client = ClientStub(eventLoop: app.eventLoopGroup.next(), data: Data.valid)
         sut = Cinamon(client: client)
 
         try sut.fetchMovies(on: app.db).wait()
 
         let movies = try Movie.query(on: app.db).with(\.$showings).all().wait()
-        XCTAssertGreaterThan(movies.count, 0)
-        movies.forEach { XCTAssertGreaterThan($0.showings.count, 0) }
+        XCTAssertEqual(movies.count, 1)
+        XCTAssertEqual(movies[0].showings.count, 1)
+
+        XCTAssertEqual(movies[0].title, "title")
+        XCTAssertEqual(movies[0].originalTitle, "originalTitle")
+        XCTAssertEqual(movies[0].year, "2020")
+        XCTAssertEqual(movies[0].duration, "81 min")
+        XCTAssertEqual(movies[0].ageRating, "N-7")
+        XCTAssertEqual(movies[0].genres, ["Animacinis"])
+
+        XCTAssertEqual(movies[0].showings[0].city, City.kaunas)
+        XCTAssertEqual(movies[0].showings[0].date, "2020-09-15 10:15:00".convertToDate())
+        XCTAssertEqual(movies[0].showings[0].venue, "Cinamon")
+        XCTAssertEqual(movies[0].showings[0].is3D, true)
+        XCTAssertEqual(movies[0].showings[0].url, "https://cinamonkino.com/mega/seat-plan/1855951372/lt")
     }
 
-    /// If response contains bad data (missing properties, incorrect values), decoder should ignore it and not throw error.
-    func testFetchingBadDataDoesNotThrow() throws {
-        let client = ClientStub(eventLoop: app.eventLoopGroup.next(), testData: .cinamonBadData)
+    func testSetsMoviePropertiesToNilIfAPIPropertiesAreMissing() throws {
+        let client = ClientStub(eventLoop: app.eventLoopGroup.next(), data: Data.invalidMovie)
         sut = Cinamon(client: client)
 
         try sut.fetchMovies(on: app.db).wait()
 
         let movies = try Movie.query(on: app.db).with(\.$showings).all().wait()
-        XCTAssertGreaterThan(movies.count, 0)
-        movies.forEach { XCTAssertGreaterThan($0.showings.count, 0) }
+        XCTAssertEqual(movies.count, 1)
+        XCTAssertEqual(movies[0].showings.count, 1)
+
+        XCTAssertEqual(movies[0].title, nil)
+    }
+
+    func testSkipsShowingIfAPIPropertiesAreMissing() throws {
+        let client = ClientStub(eventLoop: app.eventLoopGroup.next(), data: Data.invalidShowing)
+        sut = Cinamon(client: client)
+
+        try sut.fetchMovies(on: app.db).wait()
+
+        let movies = try Movie.query(on: app.db).with(\.$showings).all().wait()
+        XCTAssertEqual(movies.count, 1)
+        XCTAssertEqual(movies[0].showings.count, 0)
+    }
+
+    func testSkipsMovieIfShowShowingsIfMatchingScreenIsNotFound() throws {
+        let client = ClientStub(eventLoop: app.eventLoopGroup.next(), data: Data.invalidScreens)
+        sut = Cinamon(client: client)
+
+        try sut.fetchMovies(on: app.db).wait()
+
+        let movies = try Movie.query(on: app.db).with(\.$showings).all().wait()
+        XCTAssertEqual(movies.count, 1)
+        XCTAssertEqual(movies[0].showings.count, 0)
+    }
+
+    // MARK: Test Helpers
+
+    struct Data {
+        static var valid = """
+            {
+                "movies": [
+                    {
+                        "name": "title",
+                        "original_name": "originalTitle",
+                        "premiere_date": "2020-08-28",
+                        "runtime": 81,
+                        "rating": "N-7",
+                        "genre": {
+                            "name": "Animacinis"
+                        },
+
+                        "sessions": [
+                            {
+                                "pid": 1855951372,
+                                "screen_name": "Salė 5",
+                                "showtime": "2020-09-15 10:15:00",
+                                "is_3d": true
+                            }
+                        ]
+                    }
+                ],
+
+                "screens": [
+                    "Salė 5"
+                ]
+            }
+        """.data(using: .utf8)!
+
+        static var invalidMovie = """
+            {
+                "movies": [
+                    {
+                        "name": null,
+                        "original_name": "originalTitle",
+                        "premiere_date": "2020-08-28",
+                        "runtime": 81,
+                        "rating": "N-7",
+                        "genre": {
+                            "name": "Animacinis"
+                        },
+
+                        "sessions": [
+                            {
+                                "pid": 1855951372,
+                                "screen_name": "Salė 5",
+                                "showtime": "2020-09-15 10:15:00",
+                                "is_3d": true
+                            }
+                        ]
+                    }
+                ],
+
+                "screens": [
+                    "Salė 5"
+                ]
+            }
+        """.data(using: .utf8)!
+
+        static var invalidShowing = """
+            {
+                "movies": [
+                    {
+                        "name": null,
+                        "original_name": "originalTitle",
+                        "premiere_date": "2020-08-28",
+                        "runtime": 81,
+                        "rating": "N-7",
+                        "genre": {
+                            "name": "Animacinis"
+                        },
+
+                        "sessions": [
+                            {
+                                "pid": null,
+                                "screen_name": "Salė 5",
+                                "showtime": "2020-09-15 10:15:00",
+                                "is_3d": true
+                            }
+                        ]
+                    }
+                ],
+
+                "screens": [
+                    "Salė 5"
+                ]
+            }
+        """.data(using: .utf8)!
+
+        static var invalidScreens = """
+            {
+                "movies": [
+                    {
+                        "name": null,
+                        "original_name": "originalTitle",
+                        "premiere_date": "2020-08-28",
+                        "runtime": 81,
+                        "rating": "N-7",
+                        "genre": {
+                            "name": "Animacinis"
+                        },
+
+                        "sessions": [
+                            {
+                                "pid": 1855951372,
+                                "screen_name": "Salė 5",
+                                "showtime": "2020-09-15 10:15:00",
+                                "is_3d": true
+                            }
+                        ]
+                    }
+                ],
+
+                "screens": [
+                    "Salė 69"
+                ]
+            }
+        """.data(using: .utf8)!
     }
 }
-
