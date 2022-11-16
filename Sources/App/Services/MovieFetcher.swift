4e4827bce9047456e63066bc4d8bed0063626ffb
brien84 <houseinblues@gmail.com>
2022-11-16 21:04:40 +0200

    Implement `Apollo` service support

--- Sources/App/Services/MovieFetcher.swift
+++ Sources/App/Services/MovieFetcher.swift
@@ -13,20 +13,24 @@ protocol MovieFetching {
 }
 
 struct MovieFetcher: MovieFetching {
+    private var apollo: MovieAPI
     private var cinamon: MovieAPI
     private var forum: MovieAPI
     private var multikino: MovieAPI
 
-    init(cinamon: MovieAPI, forum: MovieAPI, multikino: MovieAPI) {
+    init(apollo: MovieAPI, cinamon: MovieAPI, forum: MovieAPI, multikino: MovieAPI) {
+        self.apollo = apollo
         self.cinamon = cinamon
         self.forum = forum
         self.multikino = multikino
     }
 
     func fetch(on db: Database) -> EventLoopFuture<Void> {
-        cinamon.fetchMovies(on: db).flatMap {
-            self.forum.fetchMovies(on: db).flatMap {
-                self.multikino.fetchMovies(on: db)
+        apollo.fetchMovies(on: db).flatMap {
+            cinamon.fetchMovies(on: db).flatMap {
+                forum.fetchMovies(on: db).flatMap {
+                    multikino.fetchMovies(on: db)
+                }
             }
         }
     }
@@ -34,6 +38,11 @@ struct MovieFetcher: MovieFetching {
 
 extension Application {
     var movieFetcher: MovieFetcher {
-        .init(cinamon: self.cinamon, forum: self.forumCinemas, multikino: self.multikino)
+        .init(
+            apollo: self.apollo,
+            cinamon: self.cinamon,
+            forum: self.forumCinemas,
+            multikino: self.multikino
+        )
     }
 }
