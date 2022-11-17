09cf4851d56addd239a2075530b5c278de5159f7
brien84 <houseinblues@gmail.com>
2022-11-17 17:11:57 +0200

    Refactor decoding logic

--- Sources/App/Services/API/Cinamon.swift
+++ Sources/App/Services/API/Cinamon.swift
@@ -18,15 +18,15 @@ struct Cinamon: MovieAPI {
     func fetchMovies(on db: Database) -> EventLoopFuture<Void> {
         client.get(apiURI).flatMap { res in
             do {
-                let service = try JSONDecoder().decode(CinamonService.self, from: res.body ?? ByteBuffer())
-                return self.createMovies(from: service, on: db)
+                let service = try JSONDecoder().decode(APIService.self, from: res.body ?? ByteBuffer())
+                return createMovies(from: service, on: db)
             } catch {
-                return self.client.eventLoop.makeFailedFuture(error)
+                return client.eventLoop.makeFailedFuture(error)
             }
         }
     }
 
-    private func createMovies(from service: CinamonService, on db: Database) -> EventLoopFuture<Void> {
+    private func createMovies(from service: APIService, on db: Database) -> EventLoopFuture<Void> {
         service.movies.map { serviceMovie in
             let movie = Movie(from: serviceMovie)
 
@@ -60,90 +60,94 @@ extension Application {
 // MARK: - Decodable Helpers
 
 extension Movie {
-    fileprivate convenience init(from cinamonMovie: CinamonMovie) {
+    fileprivate convenience init(from movie: APIService.Movie) {
         // `2020-01-01` -> `2020`
         let year: String? = {
-            guard let substring = cinamonMovie.year?.split(separator: "-").first else { return nil }
+            guard let substring = movie.year?.split(separator: "-").first else { return nil }
             return String(substring)
         }()
 
         // `69` -> `69 min`
         let duration: String? = {
-            guard let movieDuration = cinamonMovie.duration else { return nil }
+            guard let movieDuration = movie.duration else { return nil }
             return String(movieDuration) + " min"
         }()
 
         // ` Genre ` -> `["Genre"]`
         let genres: [String]? = {
-            guard let genre = cinamonMovie.genre?.name else { return nil }
+            guard let genre = movie.genre?.name else { return nil }
             return [genre.trimSpaces()]
         }()
 
-        self.init(title: cinamonMovie.title,
-                  originalTitle: cinamonMovie.originalTitle,
-                  year: year,
-                  duration: duration,
-                  ageRating: cinamonMovie.ageRating,
-                  genres: genres)
+        self.init(
+            title: movie.title,
+            originalTitle: movie.originalTitle,
+            year: year,
+            duration: duration,
+            ageRating: movie.ageRating,
+            genres: genres
+        )
     }
 }
 
 extension Showing {
-    fileprivate convenience init?(from cinamonShowing: CinamonShowing) {
-        guard let date = cinamonShowing.showtime?.convertToDate() else { return nil }
-        guard let is3D = cinamonShowing.is3D else { return nil }
-        guard let pid = cinamonShowing.pid else { return nil }
+    fileprivate convenience init?(from showing: APIService.Movie.Showing) {
+        guard let date = showing.showtime?.convertToDate() else { return nil }
+        guard let is3D = showing.is3D else { return nil }
+        guard let pid = showing.pid else { return nil }
 
-        self.init(city: .kaunas,
-                  date: date,
-                  venue: "Cinamon",
-                  is3D: is3D,
-                  url: "https://cinamonkino.com/mega/seat-plan/\(pid)/lt")
+        self.init(
+            city: .kaunas,
+            date: date,
+            venue: "Cinamon",
+            is3D: is3D,
+            url: "https://cinamonkino.com/mega/seat-plan/\(pid)/lt"
+        )
     }
 }
 
-private struct CinamonService: Decodable {
-    let movies: [CinamonMovie]
+private struct APIService: Decodable {
+    let movies: [Movie]
 
     // `screens` contain local theater IDs, which we use to filter out local showings,
     // since API returns showings from theaters across multiple countries.
     let screens: [String]
-}
 
-private struct CinamonMovie: Decodable {
-    let title: String?
-    let originalTitle: String?
-    let year: String?
-    let duration: Int?
-    let ageRating: String?
-    let genre: Genre?
-    let showings: [CinamonShowing]
+    struct Movie: Decodable {
+        let title: String?
+        let originalTitle: String?
+        let year: String?
+        let duration: Int?
+        let ageRating: String?
+        let genre: Genre?
+        let showings: [Showing]
 
-    struct Genre: Decodable {
-        let name: String
-    }
+        struct Genre: Decodable {
+            let name: String?
+        }
 
-    private enum CodingKeys: String, CodingKey {
-        case title = "name"
-        case originalTitle = "original_name"
-        case year = "premiere_date"
-        case duration = "runtime"
-        case ageRating = "rating"
-        case genre
-        case showings = "sessions"
-    }
-}
+        private enum CodingKeys: String, CodingKey {
+            case title = "name"
+            case originalTitle = "original_name"
+            case year = "premiere_date"
+            case duration = "runtime"
+            case ageRating = "rating"
+            case genre
+            case showings = "sessions"
+        }
 
-private struct CinamonShowing: Decodable {
-    let pid: Int?
-    let screen: String?
-    let showtime: String?
-    let is3D: Bool?
+        struct Showing: Decodable {
+            let pid: Int?
+            let screen: String?
+            let showtime: String?
+            let is3D: Bool?
 
-    private enum CodingKeys: String, CodingKey {
-        case pid
-        case screen = "screen_name"
-        case showtime
-        case is3D = "is_3d"
+            private enum CodingKeys: String, CodingKey {
+                case pid
+                case screen = "screen_name"
+                case showtime
+                case is3D = "is_3d"
+            }
+        }
     }
 }
