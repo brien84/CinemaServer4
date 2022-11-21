87125c3599e370fa29511dd7dcd99f38143a407c
brien84 <houseinblues@gmail.com>
2022-11-21 21:22:07 +0200

    Add legacy functionality to old routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,28 +2,49 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get("all") { req -> EventLoopFuture<[Movie]> in
+
+    app.get("all_") { req -> EventLoopFuture<[Movie]> in
         Movie.query(on: req.db).with(\.$showings).all()
     }
 
-    app.get("vilnius") { req -> EventLoopFuture<[Movie]> in
+    app.get("vilnius_") { req -> EventLoopFuture<[Movie]> in
         queryMovies(in: .vilnius, on: req)
     }
 
-    app.get("kaunas") { req -> EventLoopFuture<[Movie]> in
+    app.get("kaunas_") { req -> EventLoopFuture<[Movie]> in
         queryMovies(in: .kaunas, on: req)
     }
 
-    app.get("klaipeda") { req -> EventLoopFuture<[Movie]> in
+    app.get("klaipeda_") { req -> EventLoopFuture<[Movie]> in
         queryMovies(in: .klaipeda, on: req)
     }
 
-    app.get("siauliai") { req -> EventLoopFuture<[Movie]> in
+    app.get("siauliai_") { req -> EventLoopFuture<[Movie]> in
         queryMovies(in: .siauliai, on: req)
     }
 
-    app.get("panevezys") { req -> EventLoopFuture<[Movie]> in
+    app.get("panevezys_") { req -> EventLoopFuture<[Movie]> in
         queryMovies(in: .panevezys, on: req)
+    }
+
+    app.get("all") { req -> EventLoopFuture<[Movie]> in
+        queryAllLegacyMovies(on: req)
+    }
+
+    app.get("vilnius") { req -> EventLoopFuture<[Movie]> in
+        queryLegacyMovies(in: .vilnius, on: req)
+    }
+
+    app.get("kaunas") { req -> EventLoopFuture<[Movie]> in
+        queryLegacyMovies(in: .kaunas, on: req)
+    }
+
+    app.get("klaipeda") { req -> EventLoopFuture<[Movie]> in
+        queryLegacyMovies(in: .klaipeda, on: req)
+    }
+
+    app.get("siauliai") { req -> EventLoopFuture<[Movie]> in
+        queryLegacyMovies(in: .siauliai, on: req)
     }
 
     app.get("posters", ":fileName") { req -> Response in
@@ -38,6 +59,29 @@ func routes(_ app: Application) throws {
 
     func queryMovies(in city: City, on req: Request) -> EventLoopFuture<[Movie]> {
         Movie.query(on: req.db).with(\.$showings).all().map { movies -> [Movie] in
+            movies.forEach { movie in
+                movie.$showings.value = movie.$showings.value?.filter({ $0.city == city })
+            }
+
+            return movies.filter { $0.showings.count > 0 }
+        }
+    }
+
+    /// This function ignores `Showing`s which contain `Apollo` venue.
+    ///
+    /// This function is used in legacy apps and eventually will be removed in future release.
+    func queryAllLegacyMovies(on req: Request) -> EventLoopFuture<[Movie]> {
+        Movie.query(on: req.db).with(\.$showings).all().map { movies -> [Movie] in
+            movies.forEach { movie in
+                movie.$showings.value = movie.$showings.value?.filter({ $0.venue != "Apollo" })
+            }
+
+            return movies.filter { $0.showings.count > 0 }
+        }
+    }
+
+    func queryLegacyMovies(in city: City, on req: Request) -> EventLoopFuture<[Movie]> {
+        queryAllLegacyMovies(on: req).map { movies -> [Movie] in
             movies.forEach { movie in
                 movie.$showings.value = movie.$showings.value?.filter({ $0.city == city })
             }
