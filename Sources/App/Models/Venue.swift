959768a166e38cf506161e31ec1a40f59c8e6ac9
brien84 <houseinblues@gmail.com>
2022-12-07 20:08:10 +0200

    Add `Venue` model

---
+++ Sources/App/Models/Venue.swift
@@ -0,0 +1,16 @@
+//
+//  Venue.swift
+//  
+//
+//  Created by Marius on 2022-12-07.
+//
+
+import Foundation
+
+enum Venue: String, Codable {
+    case apollo = "Apollo"
+    case atlantis = "Atlantis"
+    case cinamon = "Cinamon"
+    case forum = "Forum Cinemas"
+    case multikino = "Multikino"
+}
