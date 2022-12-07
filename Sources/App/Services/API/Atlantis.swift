8b30623ac6879fdc342dce8cbc6cbbf00b63f5de
brien84 <houseinblues@gmail.com>
2022-12-07 15:53:51 +0200

    Implement parsing logic

--- Sources/App/Services/API/Atlantis.swift
+++ Sources/App/Services/API/Atlantis.swift
@@ -6,11 +6,12 @@
 //
 
 import Fluent
+import Parsing
 import Vapor
 
 struct Atlantis: MovieAPI {
     private let client: Client
-    
+
     init(client: Client) {
         self.client = client
     }
@@ -18,10 +19,157 @@ struct Atlantis: MovieAPI {
     func fetchMovies(on db: Database) -> EventLoopFuture<Void> {
         db.eventLoop.makeSucceededVoidFuture()
     }
+
+    private func parseShowings(from string: String) throws -> [APIParser.Showing] {
+        let string = string.components(separatedBy: "\n").joined().components(separatedBy: "\t").joined()
+
+        let scheduleBlocks = try APIParser.scheduleBlocks.parse(string)
+
+        let schedules = scheduleBlocks.compactMap { block in
+            do {
+                return try APIParser.schedule.parse(block)
+            } catch {
+                return nil
+            }
+        }
+
+        return try schedules.flatMap { schedule in
+            try schedule.showingsData.flatMap { data in
+                let showing = try APIParser.showing.parse(data)
+
+                return showing.3.map { time in
+                    APIParser.Showing(
+                        title: String(showing.0),
+                        originalTitle: String(showing.1),
+                        url: String(showing.2),
+                        time: String(time),
+                        date: schedule.date
+                    )
+                }
+            }
+        }
+    }
 }
 
 extension Application {
     var atlantis: Atlantis {
         .init(client: self.client)
+    }
+}
+
+// MARK: - Parsing Helpers
+
+private extension Movie {
+    convenience init(from showing: APIParser.Showing) {
+        self.init(
+            title: showing.title,
+            originalTitle: showing.originalTitle,
+            year: nil,
+            duration: nil,
+            ageRating: nil,
+            genres: nil
+        )
+    }
+}
+
+private extension Showing {
+    convenience init?(from showing: APIParser.Showing) {
+        guard let date = "\(showing.date) \(showing.time)".convertToDate()
+        else { return nil }
+
+        self.init(
+            city: .siauliai,
+            date: date,
+            venue: "Atlantis",
+            is3D: showing.title.contains("3D") || showing.originalTitle.contains("3D"),
+            url: "https://www.atlantiscinemas.lt/\(showing.url)"
+        )
+    }
+}
+
+private extension URI {
+    static var api: URI {
+        URI(string: "https://www.atlantiscinemas.lt/")
+    }
+}
+
+private struct APIParser {
+    struct Schedule {
+        let date: String
+        let showingsData: [String]
+
+        init(date: Substring, showingsData: [Substring]) {
+            self.date = String(date)
+            self.showingsData = showingsData.map { String($0) }
+        }
+    }
+
+    struct Showing {
+        let title: String
+        let originalTitle: String
+        let url: String
+        let time: String
+        let date: String
+    }
+
+    static let scheduleBlocks = Parse {
+        Skip { PrefixThrough("<dl class=\"tabs\" id=\"movies\">") }
+        Many {
+            PrefixUpTo("</dd>")
+        } separator: {
+            "</dd>"
+        } terminator: {
+            "</dd>"
+        }
+        Skip { Rest() }
+    }
+
+    static let schedule = Parse(Schedule.init(date:showingsData:)) {
+        Skip { PrefixThrough("<dt id=\"") }
+        PrefixUpTo("\"><span>")
+        Skip { PrefixThrough("movies_list\">") }
+        Many {
+            PrefixUpTo("</li>")
+        } separator: {
+            "</li>"
+        } terminator: {
+            "</li>"
+        }
+        Skip { Rest() }
+    }
+
+    static let showing = Parse {
+        Self.title
+        Self.originalTitle
+        Self.url
+        Self.times
+        Skip { Rest() }
+    }
+
+    private static let title = Parse {
+        Skip { PrefixThrough("><h3>") }
+        PrefixUpTo("<small>")
+    }
+
+    private static let originalTitle = Parse {
+        Skip { PrefixThrough("<small>") }
+        PrefixUpTo("</small>")
+    }
+
+    private static let url = Parse {
+        Skip { PrefixThrough("href=\"/") }
+        PrefixUpTo("\">Pirkti bilietą</a>")
+    }
+
+    private static let times = Parse {
+        Skip { PrefixThrough("class=\"sessions\"") }
+        Many {
+            Skip { PrefixThrough("<time>") }
+            PrefixUpTo("</time>")
+        } separator: {
+            "</time>"
+        } terminator: {
+            "</time>"
+        }
     }
 }
