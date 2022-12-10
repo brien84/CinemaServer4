0ef270a6d1f6118896420238fb5f1cd7774570f1
brien84 <houseinblues@gmail.com>
2022-12-10 23:42:34 +0200

    Refactor parsing logic

--- Sources/App/Services/API/Atlantis.swift
+++ Sources/App/Services/API/Atlantis.swift
@@ -20,7 +20,10 @@ struct Atlantis: MovieAPI {
         client.get(.api).flatMap { res in
             do {
                 let body = String(buffer: res.body ?? ByteBuffer())
-                let showings = try parseShowings(from: body)
+                    .components(separatedBy: "\n").joined()
+                    .components(separatedBy: "\t").joined()
+
+                let showings = try APIParser.showings.parse(body)
                 return createMovies(from: showings, on: db)
             } catch {
                 return client.eventLoop.makeFailedFuture(error)
@@ -47,36 +50,6 @@ struct Atlantis: MovieAPI {
             return db.eventLoop.makeSucceededVoidFuture()
         }
     }
-
-    private func parseShowings(from string: String) throws -> [APIParser.Showing] {
-        let string = string.components(separatedBy: "\n").joined().components(separatedBy: "\t").joined()
-
-        let scheduleBlocks = try APIParser.scheduleBlocks.parse(string)
-
-        let schedules = scheduleBlocks.compactMap { block in
-            do {
-                return try APIParser.schedule.parse(block)
-            } catch {
-                return nil
-            }
-        }
-
-        return try schedules.flatMap { schedule in
-            try schedule.showingsData.flatMap { data in
-                let showing = try APIParser.showing.parse(data)
-
-                return showing.3.map { time in
-                    APIParser.Showing(
-                        title: String(showing.0),
-                        originalTitle: String(showing.1),
-                        url: String(showing.2),
-                        time: String(time),
-                        date: schedule.date
-                    )
-                }
-            }
-        }
-    }
 }
 
 extension Application {
@@ -90,20 +63,25 @@ extension Application {
 private extension Movie {
     convenience init(from showing: APIParser.Showing) {
         self.init(
-            title: showing.title,
-            originalTitle: showing.originalTitle,
+            title: String(showing.title),
+            originalTitle: String(showing.originalTitle),
             year: nil,
             duration: nil,
-            ageRating: nil,
-            genres: nil
+            ageRating: showing.ageRating,
+            genres: showing.genres.split(separator: ",").map { String($0).trimSpaces() }
         )
     }
 }
 
 private extension Showing {
     convenience init?(from showing: APIParser.Showing) {
-        guard let date = "\(showing.date) \(showing.time)".convertToDate()
-        else { return nil }
+        guard let dateInterval = Double(showing.date) else { return nil }
+        let dateWithoutTime = Date(timeIntervalSince1970: dateInterval)
+        let calendar = Calendar.current.dateComponents([.year, .month, .day], from: dateWithoutTime)
+        guard let year = calendar.year else { return nil }
+        guard let month = calendar.month else { return nil }
+        guard let day = calendar.day else { return nil }
+        guard let date = "\(year)-\(month)-\(day) \(showing.time)".convertToDate() else { return nil }
 
         self.init(
             city: .siauliai,
@@ -122,82 +100,102 @@ private extension URI {
 }
 
 private struct APIParser {
-    struct Schedule {
-        let date: String
-        let showingsData: [String]
-
-        init(date: Substring, showingsData: [Substring]) {
-            self.date = String(date)
-            self.showingsData = showingsData.map { String($0) }
-        }
-    }
-
     struct Showing {
+        let genres: String
+        let ageRating: String
+        let date: String
         let title: String
         let originalTitle: String
         let url: String
         let time: String
-        let date: String
     }
 
-    static let scheduleBlocks = Parse {
-        Skip { PrefixThrough("<dl class=\"tabs\" id=\"movies\">") }
+    static let showings = Parse {
         Many {
-            PrefixUpTo("</dd>")
-        } separator: {
-            "</dd>"
-        } terminator: {
-            "</dd>"
-        }
-        Skip { Rest() }
-    }
-
-    static let schedule = Parse(Schedule.init(date:showingsData:)) {
-        Skip { PrefixThrough("<dt id=\"") }
-        PrefixUpTo("\"><span>")
-        Skip { PrefixThrough("movies_list\">") }
-        Many {
-            PrefixUpTo("</li>")
+            genres
+            ageRating
+            date
+            title
+            originalTitle
+            url
+            times
+            Skip { PrefixUpTo("</li>") }
         } separator: {
             "</li>"
         } terminator: {
             "</li>"
         }
         Skip { Rest() }
+    }.compactMap {
+        $0.flatMap { output in
+            let genres = output.0
+            let ageRating = output.1
+            let date = output.2
+            let title = output.3
+            let originalTitle = output.4
+            let url = output.5
+            let times = output.6
+
+            return times.map { time in
+                APIParser.Showing(
+                    genres: genres,
+                    ageRating: ageRating,
+                    date: date,
+                    title: title,
+                    originalTitle: originalTitle,
+                    url: url,
+                    time: time
+                )
+            }
+        }
     }
 
-    static let showing = Parse {
-        Self.title
-        Self.originalTitle
-        Self.url
-        Self.times
-        Skip { Rest() }
-    }
+    private static let genres = Parse {
+        Skip { PrefixThrough("class=\"genre\">") }
+        PrefixUpTo("</div><div ")
+    }.map(String.init)
+
+    private static let ageRating = Parse {
+        Skip { PrefixThrough("class=\"age-class\">") }
+        PrefixUpTo("</div><div ")
+    }.map(String.init)
+
+    private static let date = Parse {
+        Skip { PrefixThrough("sdate=") }
+        PrefixUpTo("\"")
+    }.map(String.init)
 
     private static let title = Parse {
         Skip { PrefixThrough("><h3>") }
         PrefixUpTo("<small>")
-    }
+    }.map(String.init)
 
     private static let originalTitle = Parse {
         Skip { PrefixThrough("<small>") }
         PrefixUpTo("</small>")
-    }
+    }.map(String.init)
 
     private static let url = Parse {
         Skip { PrefixThrough("href=\"/") }
         PrefixUpTo("\">Pirkti bilietą</a>")
-    }
+    }.map(String.init)
 
     private static let times = Parse {
-        Skip { PrefixThrough("class=\"sessions\"") }
         Many {
-            Skip { PrefixThrough("<time>") }
-            PrefixUpTo("</time>")
+            Parse {
+                Skip { PrefixThrough("<time>") }
+                PrefixUpTo("</time>")
+            }.map(String.init)
         } separator: {
-            "</time>"
+            Parse {
+                PrefixThrough("</div>")
+                "<div class=\"session\">"
+            }
         } terminator: {
-            "</time>"
+            Parse {
+                PrefixThrough("</div>")
+                "<div class=\"clearfix\"></div>"
+            }
         }
     }
 }
--- Tests/AppTests/Services Tests/API Tests/AtlantisTests.swift
+++ Tests/AppTests/Services Tests/API Tests/AtlantisTests.swift
@@ -35,375 +35,225 @@ final class AtlantisTests: XCTestCase {
         try sut.fetchMovies(on: app.db).wait()
 
         let movies = try Movie.query(on: app.db).with(\.$showings).all().wait()
-        XCTAssertEqual(movies.count, 5)
+        XCTAssertEqual(movies.count, 3)
         let showings = try Showing.query(on: app.db).all().wait()
-        XCTAssertEqual(showings.count, 6)
+        XCTAssertEqual(showings.count, 4)
 
         XCTAssertEqual(movies[0].title, "title3D")
         XCTAssertEqual(movies[0].originalTitle, "originalTitle")
+        XCTAssertEqual(movies[0].ageRating, "V")
+        XCTAssertEqual(movies[0].genres, ["Trileris", "Siaubo"])
 
         XCTAssertEqual(movies[0].showings[0].city, .siauliai)
-        XCTAssertEqual(movies[0].showings[0].date, "2022-12-06 18:50".convertToDate())
+        XCTAssertEqual(movies[0].showings[0].date, "2022-12-10 11:45".convertToDate())
         XCTAssertEqual(movies[0].showings[0].venue, .atlantis)
         XCTAssertEqual(movies[0].showings[0].is3D, true)
-        XCTAssertEqual(movies[0].showings[0].url, "https://www.atlantiscinemas.lt/velnio-sviesa?sdate=1670284800")
+        XCTAssertEqual(movies[0].showings[0].url, "https://www.atlantiscinemas.lt/?sdate=1670284800")
     }
 
-    func testSkipsShowingIfAPIPropertiesAreMissing() throws {
+    func testShowingWithInvalidTimeIsIgnored() throws {
         let client = ClientStub(eventLoop: app.eventLoopGroup.next(), data: Data.invalidShowing)
         sut = Atlantis(client: client)
 
         try sut.fetchMovies(on: app.db).wait()
 
         let movies = try Movie.query(on: app.db).with(\.$showings).all().wait()
-        XCTAssertEqual(movies.count, 5)
+        XCTAssertEqual(movies.count, 1)
         let showings = try Showing.query(on: app.db).all().wait()
-        XCTAssertEqual(showings.count, 5)
+        XCTAssertEqual(showings.count, 1)
     }
 
-    func testSkipsShowingIfAPIPropertiesAresMissing() throws {
+    func testParsingDoesNotFailWhenAPIContainsDateWithoutData() throws {
         let client = ClientStub(eventLoop: app.eventLoopGroup.next(), data: Data.missingDate)
         sut = Atlantis(client: client)
 
         try sut.fetchMovies(on: app.db).wait()
 
         let movies = try Movie.query(on: app.db).with(\.$showings).all().wait()
-        XCTAssertEqual(movies.count, 3)
+        XCTAssertEqual(movies.count, 1)
         let showings = try Showing.query(on: app.db).all().wait()
-        XCTAssertEqual(showings.count, 4)
+        XCTAssertEqual(showings.count, 1)
+        XCTAssertEqual(movies[0].showings[0].date, "2022-12-22 10:00".convertToDate())
     }
+
     // MARK: Test Helpers
 
     struct Data {
         static var valid = """
-        <div id="sessions_list">
         <dl class="tabs" id="movies">
-        <dt id="2022-12-06"><span><strong>Šiandien</strong><span class="month">Gruodžio</span> <span class="day">06 d.</dt>
-        <dd>
-        <ul class="movies_list">
+        <dt id="2022-12-10"><span><strong>Šiandien</strong><span class="month">Gruodžio</span> <span class="day">10 d.</span></span></dt>
+        <dd><ul class="movies_list">
 
         <li class="movie">
-        <h3>
-        title3D<small>originalTitle</small>
-        </h3>
-        </a>
-        <div class="buy-button">
-        <a class="btn btn-info" href="/velnio-sviesa?sdate=1670284800">
-        Pirkti bilietą</a>
+        <div class="movie-ticket">
+        <div class="movie-info-header">
+        <div class="genre">Trileris, Siaubo</div>
+        <div class="age-class">V</div>
+        <div class="clearfix"></div>
+        </div><a href="/lilas-lilas-krokodilas-dubliuotas-lietuviskai?sdate=1670630400""></a>
+        <div class="movie-info"><div class="movie-info-details">
+        <a href="/lilas-lilas-krokodilas-dubliuotas-lietuviskai?sdate=1670630400">
+        <h3>title3D<small>originalTitle</small></h3></a>
+        <div class="buy-button"><a class="btn btn-info" href="/?sdate=1670284800">Pirkti bilietą</a></div>
+        <div class="short-description">
+        <p>Primų šeimai persikėlus į Niujorką, jų sūnus Džošas sunkiai pritampa naujoje vietoje.</p>
+        </div>
         <div class="movie-sessions">
         <div class="sessions-title">
-        <strong>Šiandien</strong><span class="month">Gruodžio</span> <span class="day">06 d.</span>
+        <strong>Šiandien</strong><span class="month">Gruodžio</span> <span class="day">10 d.</span>
         </div>
         <div class="sessions">
         <div class="session">
-        <time>
-        18:50</time>
-        <span>
-        </span>
+        <time>11:45</time>
+        <span>Lietuvių k.</span>
+        </div>
+        <div class="session">
+        <time>14:00</time>
+        <span>Lietuvių k.</span>
+        </div>
+        <div class="clearfix"></div>
+        </div></div></div></div><div class="clearfix"></div></div>
         </li>
 
         <li class="movie">
-        <h3>
-        Negailestinga naktis<small>Violent Night</small>
-        </h3>
-        </a>
-        <div class="buy-button">
-        <a class="btn btn-info" href="/negailestinga-naktis?sdate=1670284800">
-        Pirkti bilietą</a>
+        <div class="movie-ticket">
+        <div class="movie-info-header">
+        <div class="genre">Animacinis filmas visai šeimai</div>
+        <div class="age-class">V</div>
+        <div class="clearfix"></div>
+        </div><a href="/keistas-pasaulis?sdate=1670630400" class="movie-cover" style="background:"></a>
+        <div class="movie-info"><div class="movie-info-details">
+        <a href="/keistas-pasaulis?sdate=1670630400">
+        <h3>Keistas pasaulis<small>Strange World</small></h3></a>
+        <div class="buy-button"><a class="btn btn-info" href="/?sdate=1670630400">Pirkti bilietą</a></div>
+        <div class="short-description">
+        <p>Originali kino studijos „Disney“ veiksmo kupina animacija.</p>
+        </div>
         <div class="movie-sessions">
         <div class="sessions-title">
-        <strong>Šiandien</strong><span class="month">Gruodžio</span> <span class="day">06 d.</span>
+        <strong>Šiandien</strong><span class="month">Gruodžio</span> <span class="day">10 d.</span>
         </div>
         <div class="sessions">
         <div class="session">
-        <time>
-        20:00</time>
-        <span>
-        </span>
-        </li>
-        </ul>
-        </dd>
-        <dt id="2022-12-07"><span><strong>Rytoj</strong><span class="month">Gruodžio</span> <span class="day">07 d.</dt>
-        <dd>
-        <ul class="movies_list">
-
-        <li class="movie">
-        <h3>
-        Keistas pasaulis (dubliuotas lietuviškai)<small>Strange World</small>
-        </h3>
-        </a>
-        <div class="buy-button">
-        <a class="btn btn-info" href="/keistas-pasaulis?sdate=1670371200">
-        Pirkti bilietą</a>
-        <div class="movie-sessions">
-        <div class="sessions-title">
-        <strong>Rytoj</strong><span class="month">Gruodžio</span> <span class="day">07 d.</span>
+        <time>13:20</time>
+        <span>Lietuvių k.</span>
         </div>
-        <div class="sessions">
-        <div class="session">
-        <time>
-        10:00</time>
-        <span>
-        Lietuvių k.</span>
-        </div>
-        <div class="session">
-        <time>
-        14:00</time>
-        <span>
-        Lietuvių k.</span>
+        <div class="clearfix"></div>
+        </div></div></div></div><div class="clearfix"></div></div>
         </li>
 
-        <li class="movie">
-        <h3>
-        Lilas, Lilas, Krokodilas (dubliuotas lietuviškai)<small>Lyle, Lyle, Crocodile</small>
-        </h3>
-        </a>
-        <div class="buy-button">
-        <a class="btn btn-info" href="/lilas-lilas-krokodilas-dubliuotas-lietuviskai?sdate=1670371200">
-        Pirkti bilietą</a>
-        <div class="movie-sessions">
-        <div class="sessions-title">
-        <strong>Rytoj</strong><span class="month">Gruodžio</span> <span class="day">07 d.</span>
-        </div>
-        <div class="sessions">
-        <div class="session">
-        <time>
-        11:45</time>
-        <span>
-        Lietuvių k.</span>
-        </li>
+        </ul><div class="clearfix"></div></dd>
+
+        <dt id="2022-12-11"><span><strong>Rytoj</strong><span class="month">Gruodžio</span> <span class="day">11 d.</span></span></dt>
+        <dd><ul class="movies_list">
 
         <li class="movie">
-        <h3>
-        Keistas pasaulis 3D (dubliuotas lietuviškai)<small>Strange World 3D</small>
-        </h3>
-        </a>
-        <div class="buy-button">
-        <a class="btn btn-info" href="/keistas-pasaulis-3d-dubliuotas-lietuviskai?sdate=1670371200">
-        Pirkti bilietą</a>
+        <div class="movie-ticket">
+        <div class="movie-info-header">
+        <div class="genre">Animacinis filmas visai šeimai</div>
+        <div class="age-class">V</div>
+        <div class="clearfix"></div>
+        </div><a href="/keistas-pasaulis?sdate=1670716800" class="movie-cover" style="background:"></a>
+        <div class="movie-info"><div class="movie-info-details">
+        <a href="/keistas-pasaulis?sdate=1670716800">
+        <h3>Rojaus miestas<small>Paradise City</small></h3></a>
+        <div class="buy-button"><a class="btn btn-info" href="/?sdate=1670716800">Pirkti bilietą</a></div>
+        <div class="short-description">
+        <p>Originali kino studijos „Disney“ veiksmo kupina animacija supažindina.</p>
+        </div>
         <div class="movie-sessions">
         <div class="sessions-title">
-        <strong>Rytoj</strong><span class="month">Gruodžio</span> <span class="day">07 d.</span>
+        <strong>Rytoj</strong><span class="month">Gruodžio</span> <span class="day">11 d.</span>
         </div>
         <div class="sessions">
         <div class="session">
-        <time>
-        12:00</time>
-        <span>
-        3D
-        Lietuvių k.</span>
+        <time>10:00</time>
+        <span>Lietuvių k.</span></div>
+        <div class="clearfix"></div>
+        </div></div></div></div><div class="clearfix"></div></div>
         </li>
-        </ul>
-        </dd>
-        </dl>
-        </div>
+
+        </ul><div class="clearfix"></div></dd>
         """.data(using: .utf8)!
 
         static var invalidShowing = """
-        <div id="sessions_list">
         <dl class="tabs" id="movies">
-        <dt id="2022-12-06"><span><strong>Šiandien</strong><span class="month">Gruodžio</span> <span class="day">06 d.</dt>
-        <dd>
-        <ul class="movies_list">
+
+        <dt id="2022-12-11"><span><strong>Rytoj</strong><span class="month">Gruodžio</span> <span class="day">11 d.</span></span></dt>
+        <dd><ul class="movies_list">
 
         <li class="movie">
-        <h3>
-        title3D<small>originalTitle</small>
-        </h3>
-        </a>
-        <div class="buy-button">
-        <a class="btn btn-info" href="/velnio-sviesa?sdate=1670284800">
-        Pirkti bilietą</a>
+        <div class="movie-ticket">
+        <div class="movie-info-header">
+        <div class="genre">Nuotykių filmas visai šeimai</div>
+        <div class="age-class">V</div>
+        <div class="clearfix"></div>
+        </div><a href="/lilas-lilas-krokodilas-dubliuotas-lietuviskai?sdate=1670630400""></a>
+        <div class="movie-info"><div class="movie-info-details">
+        <a href="/lilas-lilas-krokodilas-dubliuotas-lietuviskai?sdate=1670630400">
+        <h3>title3D<small>originalTitle</small></h3></a>
+        <div class="buy-button"><a class="btn btn-info" href="/?sdate=1670284800">Pirkti bilietą</a></div>
+        <div class="short-description">
+        <p>Primų šeimai persikėlus į Niujorką, jų sūnus Džošas sunkiai pritampa naujoje vietoje.</p>
+        </div>
         <div class="movie-sessions">
         <div class="sessions-title">
-        <strong>Šiandien</strong><span class="month">Gruodžio</span> <span class="day">06 d.</span>
+        <strong>Šiandien</strong><span class="month">Gruodžio</span> <span class="day">10 d.</span>
         </div>
         <div class="sessions">
         <div class="session">
-        <time>
-        SOMEVALUEFORTESTTOFAIL</time>
-        <span>
-        </span>
+        <time>INVALID-VALUE-TEST-FAIL</time>
+        <span>Lietuvių k.</span>
+        </div>
+        <div class="session">
+        <time>14:00</time>
+        <span>Lietuvių k.</span>
+        </div>
+        <div class="clearfix"></div>
+        </div></div></div></div><div class="clearfix"></div></div>
         </li>
 
-        <li class="movie">
-        <h3>
-        Negailestinga naktis<small>Violent Night</small>
-        </h3>
-        </a>
-        <div class="buy-button">
-        <a class="btn btn-info" href="/negailestinga-naktis?sdate=1670284800">
-        Pirkti bilietą</a>
-        <div class="movie-sessions">
-        <div class="sessions-title">
-        <strong>Šiandien</strong><span class="month">Gruodžio</span> <span class="day">06 d.</span>
-        </div>
-        <div class="sessions">
-        <div class="session">
-        <time>
-        20:00</time>
-        <span>
-        </span>
-        </li>
-        </ul>
-        </dd>
-        <dt id="2022-12-07"><span><strong>Rytoj</strong><span class="month">Gruodžio</span> <span class="day">07 d.</dt>
-        <dd>
-        <ul class="movies_list">
+        </ul><div class="clearfix"></div></dd>
 
-        <li class="movie">
-        <h3>
-        Keistas pasaulis (dubliuotas lietuviškai)<small>Strange World</small>
-        </h3>
-        </a>
-        <div class="buy-button">
-        <a class="btn btn-info" href="/keistas-pasaulis?sdate=1670371200">
-        Pirkti bilietą</a>
-        <div class="movie-sessions">
-        <div class="sessions-title">
-        <strong>Rytoj</strong><span class="month">Gruodžio</span> <span class="day">07 d.</span>
-        </div>
-        <div class="sessions">
-        <div class="session">
-        <time>
-        10:00</time>
-        <span>
-        Lietuvių k.</span>
-        </div>
-        <div class="session">
-        <time>
-        14:00</time>
-        <span>
-        Lietuvių k.</span>
-        </li>
-
-        <li class="movie">
-        <h3>
-        Lilas, Lilas, Krokodilas (dubliuotas lietuviškai)<small>Lyle, Lyle, Crocodile</small>
-        </h3>
-        </a>
-        <div class="buy-button">
-        <a class="btn btn-info" href="/lilas-lilas-krokodilas-dubliuotas-lietuviskai?sdate=1670371200">
-        Pirkti bilietą</a>
-        <div class="movie-sessions">
-        <div class="sessions-title">
-        <strong>Rytoj</strong><span class="month">Gruodžio</span> <span class="day">07 d.</span>
-        </div>
-        <div class="sessions">
-        <div class="session">
-        <time>
-        11:45</time>
-        <span>
-        Lietuvių k.</span>
-        </li>
-
-        <li class="movie">
-        <h3>
-        Keistas pasaulis 3D (dubliuotas lietuviškai)<small>Strange World 3D</small>
-        </h3>
-        </a>
-        <div class="buy-button">
-        <a class="btn btn-info" href="/keistas-pasaulis-3d-dubliuotas-lietuviskai?sdate=1670371200">
-        Pirkti bilietą</a>
-        <div class="movie-sessions">
-        <div class="sessions-title">
-        <strong>Rytoj</strong><span class="month">Gruodžio</span> <span class="day">07 d.</span>
-        </div>
-        <div class="sessions">
-        <div class="session">
-        <time>
-        12:00</time>
-        <span>
-        3D
-        Lietuvių k.</span>
-        </li>
-        </ul>
-        </dd>
-        </dl>
-        </div>
         """.data(using: .utf8)!
 
         static var missingDate = """
-        <div id="sessions_list">
         <dl class="tabs" id="movies">
-        <dt id="2022-12-06"><span><strong>Šiandien</strong><span class="month">Gruodžio</span> <span class="day">06 d.</dt>
+        <dt id="2022-12-09"><span><strong>Penktadienis</strong><span class="month">Gruodžio</span> <span class="day">09 d.</span></span></dt>
         <dd>
-        <div class="no-sessions">Šiuo metu negalima įsigyti bilietų 2022-12-06 dienai</div>
+        <div class="no-sessions">Šiuo metu negalima įsigyti bilietų 2022-12-09 dienai</div>
         </dd>
-        <dt id="2022-12-07"><span><strong>Rytoj</strong><span class="month">Gruodžio</span> <span class="day">07 d.</dt>
-        <dd>
-        <ul class="movies_list">
+
+        <dt id="2022-12-10"><span><strong>Rytoj</strong><span class="month">Gruodžio</span> <span class="day">10 d.</span></span></dt>
+        <dd><ul class="movies_list">
 
         <li class="movie">
-        <h3>
-        Keistas pasaulis (dubliuotas lietuviškai)<small>Strange World</small>
-        </h3>
-        </a>
-        <div class="buy-button">
-        <a class="btn btn-info" href="/keistas-pasaulis?sdate=1670371200">
-        Pirkti bilietą</a>
+        <div class="movie-ticket">
+        <div class="movie-info-header">
+        <div class="genre">Animacinis filmas visai šeimai</div>
+        <div class="age-class">V</div>
+        <div class="clearfix"></div>
+        </div><a href="/keistas-pasaulis?sdate=1671667200" class="movie-cover" style="background:"></a>
+        <div class="movie-info"><div class="movie-info-details">
+        <a href="/keistas-pasaulis?sdate=1671667200">
+        <h3>Rojaus miestas<small>Paradise City</small></h3></a>
+        <div class="buy-button"><a class="btn btn-info" href="/?sdate=1671667200">Pirkti bilietą</a></div>
+        <div class="short-description">
+        <p>Originali kino studijos „Disney“ veiksmo kupina animacija supažindina.</p>
+        </div>
         <div class="movie-sessions">
         <div class="sessions-title">
-        <strong>Rytoj</strong><span class="month">Gruodžio</span> <span class="day">07 d.</span>
+        <strong>Rytoj</strong><span class="month">Gruodžio</span> <span class="day">11 d.</span>
         </div>
         <div class="sessions">
         <div class="session">
-        <time>
-        10:00</time>
-        <span>
-        Lietuvių k.</span>
-        </div>
-        <div class="session">
-        <time>
-        14:00</time>
-        <span>
-        Lietuvių k.</span>
+        <time>10:00</time>
+        <span>Lietuvių k.</span></div>
+        <div class="clearfix"></div>
+        </div></div></div></div><div class="clearfix"></div></div>
         </li>
 
-        <li class="movie">
-        <h3>
-        Lilas, Lilas, Krokodilas (dubliuotas lietuviškai)<small>Lyle, Lyle, Crocodile</small>
-        </h3>
-        </a>
-        <div class="buy-button">
-        <a class="btn btn-info" href="/lilas-lilas-krokodilas-dubliuotas-lietuviskai?sdate=1670371200">
-        Pirkti bilietą</a>
-        <div class="movie-sessions">
-        <div class="sessions-title">
-        <strong>Rytoj</strong><span class="month">Gruodžio</span> <span class="day">07 d.</span>
-        </div>
-        <div class="sessions">
-        <div class="session">
-        <time>
-        11:45</time>
-        <span>
-        Lietuvių k.</span>
-        </li>
-
-        <li class="movie">
-        <h3>
-        Keistas pasaulis 3D (dubliuotas lietuviškai)<small>Strange World 3D</small>
-        </h3>
-        </a>
-        <div class="buy-button">
-        <a class="btn btn-info" href="/keistas-pasaulis-3d-dubliuotas-lietuviskai?sdate=1670371200">
-        Pirkti bilietą</a>
-        <div class="movie-sessions">
-        <div class="sessions-title">
-        <strong>Rytoj</strong><span class="month">Gruodžio</span> <span class="day">07 d.</span>
-        </div>
-        <div class="sessions">
-        <div class="session">
-        <time>
-        12:00</time>
-        <span>
-        3D
-        Lietuvių k.</span>
-        </li>
-        </ul>
-        </dd>
-        </dl>
-        </div>
+        </ul><div class="clearfix"></div></dd>
         """.data(using: .utf8)!
     }
 }
