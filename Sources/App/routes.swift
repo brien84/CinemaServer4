7efded0c61a4aeae611879f5539571c1cf2dd861
brien84 <houseinblues@gmail.com>
2023-10-24 08:10:02 +0300

    Optimise database queries

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,7 +8,6 @@ enum SupportedVersion: String {
 }
 
 func routes(_ app: Application) throws {
-
     app.get("posters", ":fileName") { req -> Response in
         let fileName = req.parameters.get("fileName")
         let path = "\(DirectoryConfiguration.detect().publicDirectory)Posters/" + (fileName ?? "")
@@ -16,100 +15,29 @@ func routes(_ app: Application) throws {
     }
 
     app.get(":city", ":venues") { req in
-        guard
-            let cityParameter = req.parameters.get("city"),
-            let city = City(rawValue: cityParameter)
-        else {
-            throw Abort(.badRequest)
-        }
-
-        guard let venueParameter = req.parameters.get("venues") else { throw Abort(.badRequest) }
-
-        let venues = try venueParameter
-            .split(separator: ",")
-            .map { String($0) }
-            .map {
-                guard let venue = Venue(rawValue: $0) else { throw Abort(.badRequest) }
-                return venue
-            }
-
-        return queryMovies(
-            in: [city],
-            at: venues,
-            on: req
-        )
+        let city = try getCity(req.parameters)
+        let venues = try getVenues(req.parameters)
+        return queryMovies(in: [city], at: venues, on: req)
     }
 
     app.get("vilnius", ":venues") { req -> EventLoopFuture<[Movie]> in
-        guard let venueParameter = req.parameters.get("venues") else { throw Abort(.badRequest) }
-
-        let venues = try venueParameter
-            .split(separator: ",")
-            .map { String($0) }
-            .map {
-                guard let venue = Venue(rawValue: $0) else { throw Abort(.badRequest) }
-                return venue
-            }
-
-        return queryMovies(
-            in: [.vilnius],
-            at: venues,
-            on: req
-        )
+        let venues = try getVenues(req.parameters)
+        return queryMovies(in: [.vilnius], at: venues, on: req)
     }
 
     app.get("kaunas", ":venues") { req -> EventLoopFuture<[Movie]> in
-        guard let venueParameter = req.parameters.get("venues") else { throw Abort(.badRequest) }
-
-        let venues = try venueParameter
-            .split(separator: ",")
-            .map { String($0) }
-            .map {
-                guard let venue = Venue(rawValue: $0) else { throw Abort(.badRequest) }
-                return venue
-            }
-
-        return queryMovies(
-            in: [.kaunas],
-            at: venues,
-            on: req
-        )
+        let venues = try getVenues(req.parameters)
+        return queryMovies(in: [.kaunas], at: venues, on: req)
     }
 
     app.get("klaipeda", ":venues") { req -> EventLoopFuture<[Movie]> in
-        guard let venueParameter = req.parameters.get("venues") else { throw Abort(.badRequest) }
-
-        let venues = try venueParameter
-            .split(separator: ",")
-            .map { String($0) }
-            .map {
-                guard let venue = Venue(rawValue: $0) else { throw Abort(.badRequest) }
-                return venue
-            }
-
-        return queryMovies(
-            in: [.klaipeda],
-            at: venues,
-            on: req
-        )
+        let venues = try getVenues(req.parameters)
+        return queryMovies(in: [.klaipeda], at: venues, on: req)
     }
 
     app.get("siauliai", ":venues") { req -> EventLoopFuture<[Movie]> in
-        guard let venueParameter = req.parameters.get("venues") else { throw Abort(.badRequest) }
-
-        let venues = try venueParameter
-            .split(separator: ",")
-            .map { String($0) }
-            .map {
-                guard let venue = Venue(rawValue: $0) else { throw Abort(.badRequest) }
-                return venue
-            }
-
-        return queryMovies(
-            in: [.siauliai],
-            at: venues,
-            on: req
-        )
+        let venues = try getVenues(req.parameters)
+        return queryMovies(in: [.siauliai], at: venues, on: req)
     }
 
     // MARK: v1.2 - Deprecated
@@ -129,88 +57,74 @@ func routes(_ app: Application) throws {
     }
 
     app.get("vilnius") { req -> EventLoopFuture<[Movie]> in
-        queryLegacyMovies(
-            in: [.vilnius],
-            at: [.forum, .multikino],
-            on: req
-        )
+        queryLegacyMovies(in: [.vilnius], at: [.forum, .multikino], on: req)
     }
 
     app.get("kaunas") { req -> EventLoopFuture<[Movie]> in
-        queryLegacyMovies(
-            in: [.kaunas],
-            at: [.cinamon, .forum],
-            on: req
-        )
+        queryLegacyMovies(in: [.kaunas], at: [.cinamon, .forum], on: req)
     }
 
     app.get("klaipeda") { req -> EventLoopFuture<[Movie]> in
-        queryLegacyMovies(
-            in: [.klaipeda],
-            at: [.forum],
-            on: req
-        )
+        queryLegacyMovies(in: [.klaipeda], at: [.forum], on: req)
     }
 
     app.get("siauliai") { req -> EventLoopFuture<[Movie]> in
-        queryLegacyMovies(
-            in: [.siauliai],
-            at: [.forum],
-            on: req
-        )
+        queryLegacyMovies(in: [.siauliai], at: [.forum], on: req)
     }
+}
 
-    func queryMovies(in cities: [City], at venues: [Venue], on req: Request) -> EventLoopFuture<[Movie]> {
-        Movie.query(on: req.db).with(\.$showings).all().map { movies -> [Movie] in
-            movies.forEach { movie in
-                movie.$showings.value = movie.$showings.value?.filter({ cities.contains($0.city) && venues.contains($0.venue) })
-            }
+private func getCity(_ parameters: Parameters) throws -> City {
+    guard let string = parameters.get("city") else { throw Abort(.badRequest) }
+    guard let city = City(rawValue: string) else { throw Abort(.badRequest) }
+    return city
+}
 
-            return movies.filter { $0.showings.count > 0 }
-        }
+private func getVenues(_ parameters: Parameters) throws -> [Venue] {
+    guard let string = parameters.get("venues") else { throw Abort(.badRequest) }
+    let venues = string.split(separator: ",").compactMap { Venue(rawValue: String($0)) }
+    guard !venues.isEmpty else { throw Abort(.badRequest) }
+    return venues
+}
+
+private func queryMovies(in cities: [City], at venues: [Venue], on req: Request) -> EventLoopFuture<[Movie]> {
+    Movie.query(on: req.db).with(\.$showings).with(\.$featured).all().mapEachCompact { movie in
+        movie.$showings.value = movie.$showings.value?.filter({ cities.contains($0.city) && venues.contains($0.venue) })
+        return movie.showings.count > .zero ? movie : nil
     }
+}
 
-    func queryLegacyMovies(in cities: [City], at venues: [Venue], on req: Request) -> EventLoopFuture<[Movie]> {
-        Movie.query(on: req.db).with(\.$showings).all().map { movies -> [Movie] in
-            movies.forEach { movie in
-                movie.$showings.value = movie.$showings.value?.filter({ cities.contains($0.city) && venues.contains($0.venue) })
+private func queryLegacyMovies(in cities: [City], at venues: [Venue], on req: Request) -> EventLoopFuture<[Movie]> {
+    queryMovies(in: cities, at: venues, on: req).mapEach { movie in
+        movie.showings.forEach { showing in
+            switch showing.city {
+            case .vilnius:
+                showing.city = .vilnius_
+            case .kaunas:
+                showing.city = .kaunas_
+            case .klaipeda:
+                showing.city = .klaipeda_
+            case .siauliai:
+                showing.city = .siauliai_
+            case .panevezys:
+                showing.city = .panevezys_
+            default:
+                return
             }
 
-            let movies = movies.filter { $0.showings.count > 0 }
-
-            movies.forEach { movie in
-                movie.showings.forEach { showing in
-                    switch showing.city {
-                    case .vilnius:
-                        showing.city = .vilnius_
-                    case .kaunas:
-                        showing.city = .kaunas_
-                    case .klaipeda:
-                        showing.city = .klaipeda_
-                    case .siauliai:
-                        showing.city = .siauliai_
-                    case .panevezys:
-                        showing.city = .panevezys_
-                    default:
-                        return
-                    }
-
-                    switch showing.venue {
-                    case .apollo:
-                        showing.venue = .apollo_
-                    case .cinamon:
-                        showing.venue = .cinamon_
-                    case .forum:
-                        showing.venue = .forum_
-                    case .multikino:
-                        showing.venue = .multikino_
-                    default:
-                        return
-                    }
-                }
+            switch showing.venue {
+            case .apollo:
+                showing.venue = .apollo_
+            case .cinamon:
+                showing.venue = .cinamon_
+            case .forum:
+                showing.venue = .forum_
+            case .multikino:
+                showing.venue = .multikino_
+            default:
+                return
             }
-
-            return movies
         }
+
+        return movie
     }
 }
