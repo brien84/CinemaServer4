bd13a7d064ebbfd537a6002bc89b8acaa0cade67
brien84 <houseinblues@gmail.com>
2023-10-26 19:34:08 +0300

    Implement `Featured` validation

--- Sources/App/Services/MovieValidator.swift
+++ Sources/App/Services/MovieValidator.swift
@@ -18,9 +18,31 @@ protocol MovieValidation {
 }
 
 final class MovieValidator: MovieValidation {
+    private var invalidFeatured = [Featured]()
     private var invalidMovies = [Movie]()
 
     func getReport() -> String {
+        getMovieReport()
+            .appending("<p>-----</p>")
+            .appending(getFeaturedReport())
+    }
+
+    private func getFeaturedReport() -> String {
+        var report: String
+
+        if invalidFeatured.isEmpty {
+            report = "<p>All featured passed validation!</p>"
+        } else {
+            report = "<p>Featured failed validation: </p>"
+            invalidFeatured.forEach {
+                report.append("<p>\($0.originalTitle)</p>")
+            }
+        }
+
+        return report
+    }
+
+    private func getMovieReport() -> String {
         var report: String
 
         if invalidMovies.isEmpty {
@@ -47,44 +69,49 @@ final class MovieValidator: MovieValidation {
     }
 
     func validate(on db: Database) -> EventLoopFuture<Void> {
+        invalidFeatured.removeAll()
         invalidMovies.removeAll()
 
-        let query = Movie.query(on: db).with(\.$showings) { $0.with(\.$movie) }
+        let query = Movie.query(on: db)
+            .with(\.$featured) { $0.with(\.$movie) }
+            .with(\.$showings) { $0.with(\.$movie) }
 
-        return query.all().flatMapEach(on: db.eventLoop) {
-            self.validate(movie: $0, on: db)
+        return query.all().flatMapEach(on: db.eventLoop) { movie in
+            self.validate(movie: movie, on: db).flatMap {
+                self.validate(featured: movie.featured, on: db)
+            }
+        }
+    }
+
+    private func validate(featured: Featured?, on db: Database)  -> EventLoopFuture<Void> {
+        guard let featured else { return db.eventLoop.makeSucceededFuture(()) }
+
+        do {
+            try validate(property: featured.label)
+            try validate(property: featured.title)
+            try validate(property: featured.originalTitle)
+            try validate(property: featured.startDate)
+            try validate(property: featured.endDate)
+            try validate(property: featured.imageURL)
+            return db.eventLoop.makeSucceededFuture(())
+        } catch {
+            invalidFeatured.append(featured)
+            featured.$movie.id = nil
+            return featured.update(on: db)
         }
     }
 
     private func validate(movie: Movie, on db: Database) -> EventLoopFuture<Void> {
         do {
-            let title = movie.title
-            try validate(property: title)
-
-            let originalTitle = movie.originalTitle
-            try validate(property: originalTitle)
-
-            let year = movie.year
-            try validate(property: year)
-
-            let duration = movie.duration
-            try validate(property: duration)
-
-            let ageRating = movie.ageRating
-            try validate(property: ageRating)
-
-            let genres = movie.genres
-            try validate(property: genres)
-
-            let plot = movie.plot
-            try validate(property: plot)
-
-            let poster = movie.poster
-            try validate(property: poster)
-
-            let showings = movie.showings
-            try validate(property: showings)
-
+            try validate(property: movie.title)
+            try validate(property: movie.originalTitle)
+            try validate(property: movie.year)
+            try validate(property: movie.duration)
+            try validate(property: movie.ageRating)
+            try validate(property: movie.genres)
+            try validate(property: movie.plot)
+            try validate(property: movie.poster)
+            try validate(property: movie.showings)
             return db.eventLoop.makeSucceededFuture(())
         } catch {
             invalidMovies.append(movie)
